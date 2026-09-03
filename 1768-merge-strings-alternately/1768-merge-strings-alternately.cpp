e84f9174d819b902ffaeb74d0bee5032cb215f03
Goutam Gaurabh <gautamgaurabh421@gmail.com>
2026-09-03 10:06:33 +0530

    Time: 0 ms (100%), Space: 8.4 MB (84.25%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.cpp
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.cpp
@@ -6,8 +6,10 @@ public:
         int i = 0;
         
         while (i < word1.length() || i < word2.length()) {
-            if (i < word1.length()) res += word1[i];
-            if (i < word2.length()) res += word2[i];
+            if (i < word1.length())
+             res += word1[i];
+            if (i < word2.length())
+             res += word2[i];
             i++;
         }
         
